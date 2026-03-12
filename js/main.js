50b21cb7c4856dda9799e3bdc905ab6cd65dfba0
anik-pas <anik-pas@ya.ru>
2026-03-12 13:09:43 +0300

    stop video

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 const modalWindow = document.querySelector(".modal");
 const buttonsModal = document.querySelectorAll(".button-play");
+const iframe = modalWindow.querySelector("iframe");
+const iframeSrc = iframe ? iframe.src : "";
 
 buttonsModal.forEach((item, i) => {
   item.addEventListener("click", () => {
     modalWindow.classList.add("active");
+    if (iframe && iframeSrc) {
+      iframe.src = iframeSrc;
+    }
   });
 });
 
-modalWindow.addEventListener("click", () => {
-  modalWindow.classList.remove("active");
+modalWindow.addEventListener("click", (event) => {
+  if (event.target === modalWindow) {
+    modalWindow.classList.remove("active");
+
+    if (iframe) {
+      iframe.src = "";
+    }
+  }
 });
